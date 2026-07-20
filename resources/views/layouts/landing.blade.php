8037ca63bcc128a66da217c7e3702ae7d58182e3
raydanielg <airezra2+1@gmail.com>
2026-07-20 18:23:48 +0300

    commit

--- resources/views/layouts/landing.blade.php
+++ resources/views/layouts/landing.blade.php
@@ -49,6 +49,9 @@
         }
     </script>
 
+    {{-- Font Awesome --}}
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css">
+
     {{-- Toastify --}}
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/toastify-js@1.12.0/src/toastify.min.css">
     <script src="https://cdn.jsdelivr.net/npm/toastify-js@1.12.0/src/toastify.min.js"></script>
