cc300487f5fa4ad4bad3e33cb8d60d9927ed7dac
raydanielg <airezra2+1@gmail.com>
2026-07-20 21:18:42 +0300

    commit

---
+++ resources/views/sitemap/index.blade.php
@@ -0,0 +1,32 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9"
+        xmlns:image="http://www.google.com/schemas/sitemap-image/1.1">
+    <url>
+        <loc>{{ config('app.url') }}</loc>
+        <lastmod>{{ date('Y-m-d') }}</lastmod>
+        <changefreq>weekly</changefreq>
+        <priority>1.0</priority>
+        <image:image>
+            <image:loc>{{ config('app.url') }}/logo.png</image:loc>
+            <image:title>SPESH Company Limited Logo</image:title>
+        </image:image>
+    </url>
+    <url>
+        <loc>{{ config('app.url') }}/quote</loc>
+        <lastmod>{{ date('Y-m-d') }}</lastmod>
+        <changefreq>monthly</changefreq>
+        <priority>0.8</priority>
+    </url>
+    <url>
+        <loc>{{ config('app.url') }}/login</loc>
+        <lastmod>{{ date('Y-m-d') }}</lastmod>
+        <changefreq>monthly</changefreq>
+        <priority>0.6</priority>
+    </url>
+    <url>
+        <loc>{{ config('app.url') }}/register</loc>
+        <lastmod>{{ date('Y-m-d') }}</lastmod>
+        <changefreq>monthly</changefreq>
+        <priority>0.6</priority>
+    </url>
+</urlset>
