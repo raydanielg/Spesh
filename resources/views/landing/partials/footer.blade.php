06b8d6868df1113dd5a6f9ac1da1b3e150e5fdc5
raydanielg <airezra2+1@gmail.com>
2026-07-20 18:04:46 +0300

    commit

--- resources/views/landing/partials/footer.blade.php
+++ resources/views/landing/partials/footer.blade.php
@@ -80,7 +80,7 @@
                     </li>
                     <li class="flex items-start gap-2">
                         <svg class="w-4 h-4 text-spesh-green-light flex-shrink-0 mt-0.5" fill="none" stroke="currentColor" viewBox="0 0 24 24"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M3 8l7.89 5.26a2 2 0 002.22 0L21 8M5 19h14a2 2 0 002-2V7a2 2 0 00-2-2H5a2 2 0 00-2 2v10a2 2 0 002 2z"/></svg>
-                        <span class="font-body text-sm text-white/50">info@spesh.co.tz</span>
+                        <a href="mailto:info@spesh.co.tz" class="font-body text-sm text-white/50 hover:text-spesh-green-light transition-colors">info@spesh.co.tz</a>
                     </li>
                 </ul>
             </div>
